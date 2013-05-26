07f878f42da45ba6ba68d0f808155c90c4287e3b
Ace Atienza <aceatienza@about.me>
2013-05-26 16:09:53 -0400

    add modernizr

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ requirejs.config({
 
 	"baseUrl": "js",
 	"shim": {
-		"modernizr": ["vendors/modernizr"],
+		"modernizr": ["vendors/modernizr"],				// with .load() for polyfills
 		"jquery": ["vendors/jquery-1.10.0"],
 		"hashchange": ["vendors/jquery.ba-hashchange"]
 	}
